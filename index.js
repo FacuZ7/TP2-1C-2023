2f47c3ac2aa6bcafd377d98e144f78519ac73ffd
Facundo Zapata <facundozapata41@gmail.com>
2023-04-20 20:01:57 -0300

    nueva branch y fsSincronico

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // -------------------------------    IMPORTS     -------------------------------------------- // 
+/*
 const arrCosas = require("./Utils/Cosas")
 const fnReducir = require("./Functions/reducir")
-// ------------------------------------------------------------------------------------------- //
 console.log(fnReducir(arrCosas))
+*/
+
+const fs = require("fs");
+const read = fs.readFileSync("./package.json", "UTF-8");
+console.log("🚀 ~ file: index.js:10 ~ read:", read)
+
+// ------------------------------------------------------------------------------------------- //
